f2206b128c4e9a3311d2cfe2e362a9954bd21126
Tristan F <LeoDog896@hotmail.com>
2023-08-10 10:51:28 -0400

    style: fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //! `game-solver` is a library for solving games.
-//! 
+//!
 //! If you want to read how to properly use this library,
 //! [the book](https://leodog896.github.io/game-solver/book) is
 //! a great place to start.
