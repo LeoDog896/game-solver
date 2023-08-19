15b661866792ebff70372edc7ffc4c691f942f51
Tristan F <LeoDog896@hotmail.com>
2023-08-19 18:55:31 -0400

    feat: DQN implementation

--- src/reinforcement.rs
+++ src/reinforcement.rs
@@ -1,5 +1,7 @@
 use std::collections::VecDeque;
+use candle_core::{DType, Device, Tensor};
 use rand::seq::SliceRandom;
+use candle_nn::{Linear, linear, VarBuilder, VarMap};
 use crate::game::Game;
 
 #[derive(Debug, Clone)]
@@ -35,5 +37,36 @@ impl<T: Game + Clone> ReplayMemory<T> {
 
     fn len(&self) -> usize {
         self.memory.len()
+    }
+}
+
+struct DeepQNetwork {
+    input_layer: Linear,
+    hidden_layer: Linear,
+    output_layer: Linear,
+}
+
+impl DeepQNetwork {
+    const DETAIL: usize = 128;
+
+    fn new(n_observations: usize, n_actions: usize) -> Self {
+        let varmap = VarMap::new();
+        let var_builder = VarBuilder::from_varmap(&varmap, DType::F32, &Device::Cpu);
+
+        let input_layer = linear(n_observations, Self::DETAIL, var_builder.clone()).unwrap();
+        let hidden_layer = linear(Self::DETAIL, Self::DETAIL, var_builder.clone()).unwrap();
+        let output_layer = linear(Self::DETAIL, n_actions, var_builder).unwrap();
+        DeepQNetwork {
+            input_layer,
+            hidden_layer,
+            output_layer,
+        }
+    }
+
+    fn forward(&mut self, image: Tensor) -> Tensor {
+        let mut image = image;
+        image = self.input_layer.forward(&image).unwrap().relu().unwrap();
+        image = self.hidden_layer.forward(&image).unwrap().relu().unwrap();
+        self.output_layer.forward(&image).unwrap()
     }
 }
