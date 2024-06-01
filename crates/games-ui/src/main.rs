ef0f8de88b248426ded703b710875c57d13dae1d
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-06-01 12:11:17 -0400

    chore: set up & build web app

--- crates/games-ui/src/main.rs
+++ crates/games-ui/src/main.rs
@@ -32,7 +32,7 @@ fn main() {
             .start(
                 "the_canvas_id", // hardcode it
                 web_options,
-                Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+                Box::new(|cc| Box::new(games_ui::TemplateApp::new(cc))),
             )
             .await
             .expect("failed to start eframe");
